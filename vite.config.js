7d5af9d72f89c8f6354404c33c7b260e8e7fa141
Suryansh Gupta <132469469+suryanshgupta6502@users.noreply.github.com>
2025-06-24 10:30:38 +0000

    path fix again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/suryanshgupta6502.github.io"
+  // base:"/suryanshgupta6502.github.io"
 })
